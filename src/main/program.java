0a29929e53bcff365a088bf1d67025f076042e0b
Evgeniy <elchangizo@yandex.ru>
2022-08-17 17:20:45 +0300

    Initial commit(first commit)

---
+++ src/main/program.java
@@ -0,0 +1,7 @@
+package main;
+
+public class program {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is the first version of my gitGub Test project current time is 17:15 17.08.2022");
+    }
+}

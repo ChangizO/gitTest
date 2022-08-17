a5652d830ff06f9cf01182b5dd4f9b42febe88c5
Evgeniy <elchangizo@yandex.ru>
2022-08-17 17:36:03 +0300

    Fist task was successfully solved 17:35 17.08.2022

---
+++ src/main/lesson1/HomeWork1.java
@@ -0,0 +1,11 @@
+package main.lesson1;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        task1();
+
+    }
+    private static void task1() {
+        System.out.println("Fist task was successfully solved");
+    }
+}

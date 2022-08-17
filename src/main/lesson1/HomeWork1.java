9bc9828143c0017c7c2943e8ce49cbc62603d8a6
Evgeniy <elchangizo@yandex.ru>
2022-08-17 17:53:05 +0300

    New changes 17:52

--- src/main/lesson1/HomeWork1.java
+++ src/main/lesson1/HomeWork1.java
@@ -6,6 +6,6 @@ public class HomeWork1 {
 
     }
     private static void task1() {
-        System.out.println("Fist task was successfully solved");
+        System.out.println("Fist task was successfully solved!!!!!!!!!!!");
     }
 }
